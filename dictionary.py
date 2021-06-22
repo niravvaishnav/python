5a7d935fddfc8b6d96cb01368d7e7c59061506f9
niravvaishnav <85818175+niravvaishnav@users.noreply.github.com>
2021-06-22 21:25:19 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+
+mydict = {
+    'Fast': 'In a quick manner',
+    'Marks': [92, 96, 95],
+    'Tuple': (1, 2, 3)
+}
+# mydict.clear()
+# print(mydict)
+
+# copydict = mydict.copy()
+# print(copydict)
+
+# x = ('Square', 'Rectangle')
+# y = 'Polygon'
+# anotherdict = dict.fromkeys(x, y)
+# print(anotherdict)
+
+# print(mydict.get('Tuple'))
+
+# print(mydict.items())
+
+# print(mydict.keys())
+
+# mydict.pop('Marks')
+# print(mydict)
+
+# mydict.popitem()
+# print(mydict)
+
+# print(mydict.setdefault('Fast'))
+
+# mydict.update({'Tortoise': 'Slow'})
+# print(mydict)
+
+# print(mydict.values())
---
+++ lists.py
@@ -0,0 +1,34 @@
+
+l1 = [4, 18, 24, 56, 4, 82]
+l2 = [1, 2, 5, 6, 9]
+
+# l1.append(8)
+# print(l1)
+
+# l1.clear()
+# print(l1)
+
+# l3 = l1.copy()
+# print(l3)
+
+# print(l1.count(4))
+
+# l1.extend(l2)
+# print(l1)
+
+# print(l1.index(4))
+
+# l1.insert(2, 60)
+# print(l1)
+
+# l1.pop(3)
+# print(l1)
+
+# l1.remove(24)
+# print(l1)
+
+# l1.reverse()
+# print(l1)
+
+# l1.sort()
+# print(l1)
